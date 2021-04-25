b6fdb9615e967adfb1f607c487a502bb4434dea6
Haruhiko Takada <haruhiko.takada@gmail.com>
2021-04-26 00:12:55 +0900

    最初にBotがリアクションを付ける

--- main.py
+++ main.py
@@ -47,7 +47,8 @@ async def on_message(message):
         # 募集文掲載
         recruiting = content[len(COMMAND):]
         body = "%s\n%s" % (recruiting, BODY_TEXT)
-        await message.channel.send(body)
+        message = await message.channel.send(body)
+        await message.add_reaction('👍')
     elif content == COMMAND:
         # ヘルプ表示
         body = HELP_MESSAGE
@@ -69,11 +70,12 @@ async def on_reaction_update(reaction, user):
     # 編集後のメッセージ文字列を生成して
     lines = message.content.splitlines()
     content = "%s\n%s" % (lines[0], lines[1])
-    if reaction.count >= 1:
+    if reaction.count >= 2:
         content += "\n\n"
         content += COUNT_TEXT % reaction.count
         async for user in reaction.users():
-            content += "%s\n" % user.mention
+            if user != client.user:
+                content += "%s\n" % user.mention
     # メッセージを編集する
     await message.edit(content=content)
 
